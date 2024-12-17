66a9dadd4cd6930a9294e23bc4549c64d941eeb9
Batuhan Türkyılmaz <2001.batuhan.28@gmail.com>
2024-12-18 00:43:38 +0300

    From here on, and gate has been completed.    *minor changes might be needed.    -all of the problems has been solved.

--- src/main/java/net/javaprojesi/logicgate/block/AndGateBlock.java
+++ src/main/java/net/javaprojesi/logicgate/block/AndGateBlock.java
@@ -12,7 +12,7 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
-
+//and
 public class AndGateBlock extends Block {
     // Giriş ve çıkış durumları
     public static final BooleanProperty INPUT1 = BooleanProperty.create("input1");
